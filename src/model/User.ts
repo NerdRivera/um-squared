d4c71f70d869d6072e4ee6e3cccf091ad6e10da7
Gabriel Rivera <gaberivera.br@gmail.com>
2026-06-20 17:08:55 -0400

    refactor:change User interfaces for consistency

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,17 +1,21 @@
+export type User = RegularUser | AdminUser | OrganizationUser;
 export interface RegisteredUser {
     id: string;
     username: string;
     email: string;
-    passwordHash: string;
     createdAt: Date;
     updatedAt: Date;
+    role: 'user' | 'admin' | 'organization';
+}
+export interface RegularUser extends RegisteredUser {
+    role: 'user';
 }
 
 export interface AdminUser extends RegisteredUser {
     role: 'admin';
 }
 
-export interface organizationUser extends RegisteredUser {
+export interface OrganizationUser extends RegisteredUser {
     role: 'organization';
     organizationId: string;
 }
